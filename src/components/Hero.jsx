2ae53daf37ac452c3a7585594698e9316f2754b9
Manu021108 <manishpandey021108@gmail.com>
2026-02-03 15:55:35 +0530

    Name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Manish Sahu Kumar</span>
+            Hi, I'm <span className='text-[#915EFF]'>Manish Kumar Sahu </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Building intelligent, scalable web & AI systems <br className='sm:block hidden' />
